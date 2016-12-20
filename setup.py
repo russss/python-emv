af2eedc90685b0805593186438d511345cccd78d
Russ Garrett <russ@garrett.co.uk>
2016-12-20 17:22:08 +0000

    Add license

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(name='emv',
       version='0.1',
       description='EMV Smartcard Protocol Library',
+      license='MIT',
       author='Russ Garrett',
       author_email='russ@garrett.co.uk',
       packages=[b'emv', b'emv.protocol'],
