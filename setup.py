fe305ad9346dacdd31e764ab9f1401c9b0eb4d5e
Russ Garrett <russ@garrett.co.uk>
2016-12-21 01:06:07 +0000

    Version -> 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='emv',
-      version='0.1',
+      version='0.2',
       description='EMV Smartcard Protocol Library',
       license='MIT',
       author='Russ Garrett',
