cd7a4020c9f83883392a8369d89f82c4008c782e
Russ Garrett <russ@garrett.co.uk>
2019-05-12 12:17:20 +0100

    Reformat missed file

--- setup.py
+++ setup.py
@@ -4,34 +4,38 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 version = {}
-with open(path.join(here, 'emv', "__init__.py")) as fp:
+with open(path.join(here, "emv", "__init__.py")) as fp:
     exec(fp.read(), version)
 
-setup(name='emv',
-      version=version['__version__'],
-      description='EMV Smartcard Protocol Library',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      license='MIT',
-      author='Russ Garrett',
-      author_email='russ@garrett.co.uk',
-      url='https://github.com/russss/python-emv',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3',
-      ],
-      keywords='smartcard emv payment',
-      python_requires='>=3.4',
-      packages=['emv', 'emv.protocol', 'emv.command'],
-      install_requires=['enum-compat', 'argparse', 'pyscard', 'pycountry', 'terminaltables', 'click'],
-      entry_points={
-          'console_scripts': {
-              'emvtool=emv.command.client:run'
-          }
-      }
-      )
+setup(
+    name="emv",
+    version=version["__version__"],
+    description="EMV Smartcard Protocol Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Russ Garrett",
+    author_email="russ@garrett.co.uk",
+    url="https://github.com/russss/python-emv",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="smartcard emv payment",
+    python_requires=">=3.4",
+    packages=["emv", "emv.protocol", "emv.command"],
+    install_requires=[
+        "enum-compat",
+        "argparse",
+        "pyscard",
+        "pycountry",
+        "terminaltables",
+        "click",
+    ],
+    entry_points={"console_scripts": {"emvtool=emv.command.client:run"}},
+)
